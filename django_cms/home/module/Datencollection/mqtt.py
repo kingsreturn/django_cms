df6aec44a7e30d468d5ce352e6c67218e4b702ad
wenfeng <wenfeng.yang@mailbox.tu-dresden.de>
2021-05-10 10:30:38 +0200

    add mqtt subscribe and generate datasets

--- django_cms/home/module/Datencollection/mqtt.py
+++ django_cms/home/module/Datencollection/mqtt.py
@@ -1,36 +1,63 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
-from AbstractClass import Collection
-from Datamodel import DataModel
+#from AbstractClass import Collection
+#from Datamodel import DataModel
+from queue import Queue
+from django.core.cache import cache
+import json
+import numpy as np
 
-
-class mqtt(Collection):
-    def __init__(self):
+class Mqtt():
+    def __init__(self,address):
         self.protocol='mqtt'
-        self.server = 'localhoast'
+        self.server = '8.140.157.208'
         self.port=8083
-        self.address="testtopic/#"
+        self.address=address
         self.client = mqtt.Client(transport="websockets")
         self.client.on_connect = on_connect
-        self.client.on_message = on_message
+        self.client.on_message = self.on_message
+        self.Data=list(range(0,100,1))
+        self.interval=3
+        self.count = 0
+        cache.set(address, self.Data, 30)
+        cache.set(self.address + '/value', 1, 30)
+
 
     def ConnectBroker(self):
         result = self.client.connect(self.address,self.port,60)
         return result
 
     def GetData(self):
-
         pass
+
+    def on_message(self,client, userdata, msg):
+        times = self.interval / 0.1
+        self.count+=1
+
+        Drehmoment = float(msg.payload.decode("utf-8"))
+        if self.count==30:
+            self.count=0
+            del self.Data[0:30]
+            cache.set(self.address,self.Data,10)
+            print(self.address,' is updated!')
+            #print(self.Data)
+            #self.Data.clear()
+            cache.set(self.address+'/value',Drehmoment,10)
+
+        self.Data.append(Drehmoment)
+        #self.StoreDataPoint(Drehmoment, '', 'Drehmoment')
+        return str(msg.payload.decode("utf-8"))
 
 # 连接的回调函数
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe("$SYS/#")
+    #client.subscribe("$SYS/#")
+
+
 
 
 # 收到消息的回调函数
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+
 
 def on_publish(client, obj, mid):
     print("mid: " + str(mid))
@@ -43,13 +70,19 @@ def on_subscribe(client, obj, mid, granted_qos):
 def on_log(client, obj, level, string):
     print(string)
 
+if __name__ == '__main__':
+    mqtt_client = Mqtt("/test/sin")
+    mqtt_client.client.connect("8.140.157.208", 8083, 60)
+    mqtt_client.client.subscribe("/test/sin", 0)
+    mqtt_client.client.loop_forever()
 
-client = mqtt.Client(transport="websockets")
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("localhost", 8083, 60)
-client.subscribe("testtopic/#", 0)
-client.loop_forever()
-#msg = subscribe.simple("testtopic/#", hostname="localhost:8083")
-#print('success')
-#print(f"{msg.topic} {msg.payload}")
+
+    #client = mqtt.Client(transport="websockets")
+    #client.on_connect = on_connect
+    #client.on_message = on_message
+    #client.connect("8.140.157.208", 8083, 60)
+    #client.subscribe("/test/motor", 0)
+    #client.loop_forever()
+    #msg = subscribe.simple("testtopic/#", hostname="localhost:8083")
+    #print('success')
+    #print(f"{msg.topic} {msg.payload}")
--- django_cms/home/module/Datencollection/test_mqtt.py
+++ django_cms/home/module/Datencollection/test_mqtt.py
@@ -1,37 +1,60 @@
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
-from Influxdb import Influxdb
-from Datamodel import DataModel
-from AbstractClass import Collection
+#from AbstractClass import Collection
+#from Datamodel import DataModel
+from queue import Queue
+from django.core.cache import cache
+import json
+import numpy as np
 
-
-class mqtt_collection(Collection):
-    def __init__(self):
-        self.protocol = 'mqtt'
-        self.server = 'localhoast'
-        self.port = 8083
-        self.address = "testtopic/#"
+class Mqtt():
+    def __init__(self,address):
+        self.protocol='mqtt'
+        self.server = '8.140.157.208'
+        self.port=8083
+        self.address=address
         self.client = mqtt.Client(transport="websockets")
         self.client.on_connect = on_connect
-        self.client.on_message = on_message
+        self.client.on_message = self.on_message
+        self.Data=[]
+        self.count=0
+        self.Data= list(range(0,100,1))
+        #cache.set(address, np.ones(100), 30)
+        #cache.set(self.address + '/value', 1, 30)
+
 
     def ConnectBroker(self):
-        result = self.client.connect(self.address, self.port, 60)
+        result = self.client.connect(self.address,self.port,60)
         return result
 
     def GetData(self):
         pass
 
+    def on_message(self,client, userdata, msg):
+        self.count+=1
+        Drehmoment = float(msg.payload.decode("utf-8"))
+        if self.count==30:
+            self.count = 0
+            del self.Data[0:30]
+            #cache.set(self.address,self.Data,10)
+            print(self.address,' is updated!')
+            print(self.Data)
+            #self.Data.clear()
+            #cache.set(self.address+'/value',Drehmoment,10)
+        #np.append(self.Data,[Drehmoment],axis=0)
+        self.Data.append(Drehmoment)
+        #self.StoreDataPoint(Drehmoment, '', 'Drehmoment')
+        return str(msg.payload.decode("utf-8"))
 
 # 连接的回调函数
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe("$SYS/#")
+    #client.subscribe("$SYS/#")
+
+
 
 
 # 收到消息的回调函数
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
 
 
 def on_publish(client, obj, mid):
@@ -45,13 +68,19 @@ def on_subscribe(client, obj, mid, granted_qos):
 def on_log(client, obj, level, string):
     print(string)
 
+if __name__ == '__main__':
+    mqtt_client = Mqtt("/test/sin")
+    mqtt_client.client.connect("8.140.157.208", 8083, 60)
+    mqtt_client.client.subscribe("/test/sin", 0)
+    mqtt_client.client.loop_forever()
 
-client = mqtt.Client(transport="websockets")
-client.on_connect = on_connect
-client.on_message = on_message
-client.connect("localhost", 8083, 60)
-client.subscribe("/test/server", 0)
-client.loop_forever()
-# msg = subscribe.simple("testtopic/#", hostname="localhost:8083")
-# print('success')
-# print(f"{msg.topic} {msg.payload}")
+
+    #client = mqtt.Client(transport="websockets")
+    #client.on_connect = on_connect
+    #client.on_message = on_message
+    #client.connect("8.140.157.208", 8083, 60)
+    #client.subscribe("/test/motor", 0)
+    #client.loop_forever()
+    #msg = subscribe.simple("testtopic/#", hostname="localhost:8083")
+    #print('success')
+    #print(f"{msg.topic} {msg.payload}")
